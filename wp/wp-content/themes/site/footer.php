f55b0ce88c22ee10bb813b623fef66ebc6817726
Eric <eric@xtunes.cc>
2011-07-14 18:30:40 +0800

    弹出框样式

--- wp/wp-content/themes/site/footer.php
+++ wp/wp-content/themes/site/footer.php
@@ -23,32 +23,35 @@
 				</div>
 			</div>
 		</div>
+</div>
 	<div class="overlay">  		
   	</div>
   	<div class="imgbox">
+  		<div id="outerImageContainer">
   			<div class="fullimg">
   				<img src="/images/1.jpg">
+  				<div class="name">
+  				<a href="#">INFO</a> -
+  				<a href="#">ZOOM</a> -
+  				<a href="#">VIDEO</a>
   			</div>
-  			<div class="name">
-  			<p>J’ai peur- Sasions, L’automn est avance</p>
-			<p>C- Prints, technique mixte</p>
-			<p>90x120cm, 2011</p>
-			<p>Limité de 6</p>
-			<p>Heart demon- Seasons, Deep autumn</p>
-			<p>C- Prints, Mixed media</p>
-			<p>90x120cm, 2011</p>
-			<p>Limited of 6</p>
-			<p>心魔 -四季之深秋</p>
-			<p>数码微喷， 混合媒体</p>
-			<p>2011， Limited of 6</p>
-  		</div>
-  		<div class="name">
-  			<a href="#">INFO</a> -
-  			<a href="#">ZOOM</a> -
-  			<a href="#">VIDEO</a>
-  		</div>
+  			</div>
+  			<div class="imgintro">
+  				<p>J’ai peur- Sasions, L’automn est avance</p>
+				<p>C- Prints, technique mixte</p>
+				<p>90x120cm, 2011</p>
+				<p>Limité de 6</p>
+				<p>Heart demon- Seasons, Deep autumn</p>
+				<p>C- Prints, Mixed media</p>
+				<p>90x120cm, 2011</p>
+				<p>Limited of 6</p>
+				<p>心魔 -四季之深秋</p>
+				<p>数码微喷， 混合媒体</p>
+				<p>2011， Limited of 6</p>
+  			</div>
+  			<div class="clear"></div> 
+  		</div>  			
   	</div>
-
 <?php
 	/* Always have wp_footer() just before the closing </body>
 	 * tag of your theme, or you will break many plugins, which
@@ -57,5 +60,6 @@
 
 	wp_footer();
 ?>
+
 </body>
 </html>
